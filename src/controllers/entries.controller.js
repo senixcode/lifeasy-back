ce296db4b8d63b1b47df538ace51f3cdcd95b275
Luis Romero <codesenix@gmail.com>
2022-11-03 09:44:07 -0500

    add controllerDetail:isFindAllById in Entry

--- src/controllers/entries.controller.js
+++ src/controllers/entries.controller.js
@@ -1,12 +1,14 @@
 import EntriesModel from "../models/Entries.model.js"
 import RateController from "../controllers/rate.controller.js"
 import CategoryController from "../controllers/category.controller.js"
+import DetailController from "../controllers/details.controller.js"
 
 class Entry {
     constructor() {
         this.model = EntriesModel
         this.controllerRate = new RateController()
         this.controllerCategory = new CategoryController()
+        this.controllerDetail = new DetailController()
     }
 
     async list(_, res) {
@@ -17,14 +19,18 @@ class Entry {
             res.status(400).json({ statusMessage: error })
         }
     }
+
     async create(req, res) {
         try {
-            const { rates, categories } = req.body
+            const { rates, categories, details } = req.body
+
             const ratesFindById = await this.controllerRate.isFindAllById(rates)
             const categoriesFindById = await this.controllerCategory.isFindAllById(categories)
+            const detailsFindById = await this.controllerDetail.isFindAllById(details)
 
             if (ratesFindById) res.json({ message: "no found rates by Id", rates })
             if (categoriesFindById) res.json({ message: "no found categories by Id", categories })
+            if (detailsFindById) res.json({ message: "no found details by Id", details })
 
             const record = new this.model(req.body)
             await record.save()
@@ -33,15 +39,19 @@ class Entry {
             res.status(400).json({ statusMessage: error.message })
         }
     }
+
     async update(req, res) {
         try {
             const { id } = req.params
-            const { rates, categories } = req.body
+            const { rates, categories, details } = req.body
+
             const ratesFindById = await this.controllerRate.isFindAllById(rates)
             const categoriesFindById = await this.controllerCategory.isFindAllById(categories)
+            const detailsFindById = await this.controllerDetail.isFindAllById(details)
 
             if (ratesFindById) res.json({ message: "no found rates by Id", rates })
             if (categoriesFindById) res.json({ message: "no found categories by Id", categories })
+            if (detailsFindById) res.json({ message: "no found details by Id", details })
 
             let updated = await this.model.findOneAndUpdate(id, req.body, {
                 new: true
@@ -51,6 +61,7 @@ class Entry {
             res.status(400).json({ statusMessage: error })
         }
     }
+    
     async delete(req, res) {
         try {
             const { id } = req.params
